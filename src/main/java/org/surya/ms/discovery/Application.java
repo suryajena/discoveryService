9442195cc9de87e9f777714b472400acd2325066
surya <surya.jena@yahoo.com>
2017-04-12 20:29:15 -0400

    Discovery service : Initial commit

---
+++ src/main/java/org/surya/ms/discovery/Application.java
@@ -0,0 +1,16 @@
+package org.surya.ms.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+ 
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+	
+	public static void main(final String... args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
